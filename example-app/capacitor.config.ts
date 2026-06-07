b2f2835ad4cc29d730aefb354d3aa84a0f423e81
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:46 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,25 +3,25 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "app.capgo.sslpinning.example",
-  "appName": "SSL Pinning Example",
-  "webDir": "dist",
-  "plugins": {
-    "CapacitorHttp": {
-      "enabled": true
+  appId: 'app.capgo.sslpinning.example',
+  appName: 'SSL Pinning Example',
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
     },
-    "SplashScreen": {
-      "launchAutoHide": false
+    SplashScreen: {
+      launchAutoHide: false,
     },
-    "CapacitorUpdater": {
-      "appId": "app.capgo.sslpinning.example",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+    CapacitorUpdater: {
+      appId: 'app.capgo.sslpinning.example',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
